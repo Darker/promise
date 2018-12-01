6b1dc7053284a67a1d94d89a2d65dd1ca910e3c9
Jakub Mareda <mareda@techsys.cz>
2018-12-01 19:39:06 +0100

    Add throws to allow non-runtime exception

--- src/main/java/io/github/darker/promise/PromiseCallbackCatch.java
+++ src/main/java/io/github/darker/promise/PromiseCallbackCatch.java
@@ -1,20 +1,20 @@
 package io.github.darker.promise;
 public interface PromiseCallbackCatch<TReturnFromCatch> {
-	TReturnFromCatch catchException(Throwable e);
+	TReturnFromCatch catchException(Throwable e) throws Throwable;
 	
     public static interface VoidReturn extends PromiseCallbackCatch<java.lang.Void> {
-    	public default java.lang.Void catchException(Throwable e) {
+    	public default java.lang.Void catchException(Throwable e) throws Throwable {
     		catchExceptionVoid(e);
     		return null;
     	}
-    	void catchExceptionVoid(Throwable e);
+    	void catchExceptionVoid(Throwable e) throws Throwable;
     }
     public static interface IgnoreReturn<TReturnFromCatch> extends PromiseCallbackCatch<TReturnFromCatch> {
-    	public default TReturnFromCatch catchException(Throwable e) {
+    	public default TReturnFromCatch catchException(Throwable e) throws Throwable {
     		catchExceptionVoid(e);
     		return null;
     	}
-    	void catchExceptionVoid(Throwable e);
+    	void catchExceptionVoid(Throwable e) throws Throwable;
     }
 }
 //public interface PromiseCallbackCatch<TException extends Throwable> {
--- src/main/java/io/github/darker/promise/PromiseCallbackThen.java
+++ src/main/java/io/github/darker/promise/PromiseCallbackThen.java
@@ -1,19 +1,19 @@
 package io.github.darker.promise;
 
 public interface PromiseCallbackThen<ArgumentType, ReturnType> {
-    ReturnType processValue(ArgumentType value);
+    ReturnType processValue(ArgumentType value) throws Throwable;
     
     public static interface VoidReturn<ArgumentType> extends PromiseCallbackThen<ArgumentType, java.lang.Void> {
-    	public default java.lang.Void processValue(ArgumentType value) {
+    	public default java.lang.Void processValue(ArgumentType value) throws Throwable {
     		processValueVoid(value);
     		return null;
     	}
-    	void processValueVoid(ArgumentType value);
+    	void processValueVoid(ArgumentType value) throws Throwable;
     }
     public static interface VoidArgument<ReturnType> extends PromiseCallbackThen<java.lang.Void, ReturnType> {
-    	public default ReturnType processValue(java.lang.Void value) {
+    	public default ReturnType processValue(java.lang.Void value) throws Throwable {
     		return processValueVoid();
     	}
-    	ReturnType processValueVoid();
+    	ReturnType processValueVoid() throws Throwable;
     }
 }
